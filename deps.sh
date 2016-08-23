06dc35d925575f93948442bab2d08687f6d8a816
fcard <ficarde@gmail.com>
2016-08-22 21:40:55 -0300

    Fix restnote.sh not finding the install directory
    Also:
     - fix a bug in powscript and recompile the scripts.
     - add versioning
     - reorganize the script to shift dependency locating from restnote.sh to deps.sh

--- deps.sh
+++ deps.sh
@@ -9,6 +9,20 @@ tmpfile="/tmp/$(basename $0).tmp.$(whoami)"
 
 
 
+VERSION="0.1.1"
+
+VersionLocation=~/.restnote/VERSION
+
+if [[ -d ~/.restnote ]]; then
+  if [[ -e "$VersionLocation" ]]; then
+    INSTALLED_VERSION="$(cat < "$VersionLocation")"
+  else
+    INSTALLED_VERSION="0.1.0"
+  fi
+else
+  INSTALLED_VERSION="0.0.0"
+fi
+
 if [[ ! "$(uname -o)" == "GNU/Linux" ]]; then
   echo -e "\nCurrently only Linux is supported.\n"
   exit 1
@@ -41,7 +55,7 @@ ask(){
     elif [[ "$answer" =~ ([Nn]o?|NO) ]]; then
       return 1
     elif [[ "$answer" == "" ]]; then
-      if [[ "$default" == n ]]; then
+      if [[ "$default" == y ]]; then
         return 0
       else
         return 1
@@ -52,14 +66,34 @@ ask(){
   fi
 }
 
-
-if [[ -d ~/.restnote ]]; then
-  if ask "\nDo you want to reinstall the restnote dependencies?" n; then
-    echo
+add_nw_node_links(){
+  if [[ -x "$(command -v node)" ]]; then
+    ln -s "$(command -v node)" ~/.restnote/bin/node
   else
+    ln -s ~/.restnote/node-*/bin/node ~/.restnote/bin/node
+  fi
+  if [[ -x "$(command -v nw)" ]]; then
+    ln -s "$(command -v nw)" ~/.restnote/bin/nw
+  else
+    ln -s ~/.restnote/node_modules/nw/bin/nw ~/.restnote/bin/nw
+  fi
+}
+
+
+if [[ ! "$VERSION" == "0.0.0" ]]; then
+  if [[ "$INSTALLED_VERSION" == "$VERSION" ]]; then
+    if ask "\nDo you want to reinstall the restnote dependencies?" n; then
+      echo
+    else
+      exit 0
+    fi
+  elif [[ "$INSTALLED_VERSION" == "0.1.0" ]]; then
+    echo "Upgrading..."
+    add_nw_node_links
+    echo "Done!"
+    echo "$VERSION" > "$VersionLocation"
     exit 0
   fi
-  rm -r ~/.restnote
 else
   echo -e "\nThe following programs will be installed:"
   echo -e "Globally:"
@@ -95,13 +129,16 @@ fi
 echo -e "\nInstalling dependencies...\n"
 cd ~
 
-if [[ ! -x .restnote ]]; then
-  mkdir .restnote || exit 1
+if [[ -d .restnote ]]; then
+  rm -r .restnote
 fi
 
+mkdir .restnote || exit 1
 cd .restnote
 
-if [[ ! -x bin ]]; then
+echo "0.0.0" > "$VersionLocation"
+
+if [[ ! -d bin ]]; then
   mkdir bin || exit 1
 fi
 
@@ -109,25 +146,25 @@ if [[ ! -x "$(command -v xz)" ]]; then
   echo "Downloading xz..."
   wget "http://tukaani.org/xz/xz-5.2.2.tar.gz" -O "xz.tar.gz"
   tar -xvf "xz.tar.gz"
-  cd xz
+  cd xz-*
   ./configure
   make
   cd ..
-  unxz="xz/bin/unxz"
+  XZ=~/xz-*/src/xz/xz
 else
-  unxz="unxz"
+  XZ="xz"
 fi
 
 if [[ ! -x "$(command -v npm)" ]]; then
-  if [[ ! -x ./node-v* ]]; then
+  if [[ ! -d ./node-v* ]]; then
     echo "Downloading node.js..."
     wget "https://nodejs.org/dist/v6.3.0/node-v6.3.0-linux-x64.tar.xz" -O "node.tar.xz"
-    "$unxz" "node.tar.xz"
+    "$XZ" -d "node.tar.xz"
     tar -xvf "node.tar"
     rm "node.tar"
   fi
-  if [[ ! -x ./node_modules/nwjs ]]; then
-    echo -e "\nInstalling nw.js locally..."
+  if [[ ! -d ./node_modules/nw ]]; then
+    echo -e "\nInstalling nw.js locally... (might take a while)"
     mkdir "node_modules"
     ./node-v*/bin/node ./node-v*/bin/npm install "nw"
   fi
@@ -157,7 +194,7 @@ if [[ "$installpip" == "yes" ]]; then
   if [[ ! -x "$(command -v 'python3')" ]]; then
     echo -e "\nDownloading python 3 with pip..."
     wget "https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tar.xz" -O "python3.tar.xz"
-    "$unxz" "python3.tar.xz"
+    "$XZ" -d "python3.tar.xz"
     tar -xvf "python3.tar"
     rm "python3.tar"
     mkdir python3
@@ -200,6 +237,9 @@ fi
 ln -s "${PYTHON_BINDIR}/restview" ~/.restnote/bin/restview
 ln -s "${PYTHON_BINDIR}/jupyter"  ~/.restnote/bin/jupyter
 ln -s "$(readlink -m "$(dirname "$0")")/restnote.sh"  ~/.restnote/bin/restnote
+add_nw_node_links
+
+echo "$VERSION" > "$VersionLocation"
 
 echo -e "\nInstallation sucessful! You may now run restnote with '$(dirname "$0")/restnote.sh'"
 
@@ -210,8 +250,8 @@ exit 0
 
 
 # cleanup tmp files
-if ls /tmp/$(basename $0).tmp.fabio* &>/dev/null; then
-  for f in /tmp/$(basename $0).tmp.fabio*; do rm $f; done
+if ls /tmp/$(basename $0).tmp.$(whoami)* &>/dev/null; then
+  for f in /tmp/$(basename $0).tmp.$(whoami)*; do rm $f; done
 fi
 
 exit 0
